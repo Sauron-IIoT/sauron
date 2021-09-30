92ff408201eb123b9ba44589461e8ff06f5e98a9
Murilo Kakazu <yuri.kakazu@gmail.com>
2021-09-30 06:59:11 +0000

    feat: count labels separately

--- app/repository/helix.py
+++ app/repository/helix.py
@@ -11,14 +11,20 @@ headers_post = {
 
 
 def create_capture(capture):
+
+    entity_type = capture['prediction_label']
+
+    if entity_type == None:
+        entity_type = 'prediction_failure'
+
     helix_capture = {
         'uuid': capture['id'],
         'captured_at': capture['captured_at'],
-        'prediction_label': capture['prediction_label'],
+        'prediction_label': entity_type,
         'prediction_confidence': capture['prediction_confidence']
     }
     helix_capture['id'] = f'urn:ngsi-ld:capture:{capture["id"]}'
-    helix_capture['type'] = 'capture'
+    helix_capture['type'] = entity_type
 
     response = requests.post(f'{settings["broker_address"]}/v2/entities?options=keyValues',
                              data=json.dumps(helix_capture), headers=headers_post)
