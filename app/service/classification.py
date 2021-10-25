555fa35d4e9ed5ac8262f47bdd2cb9af2b2681ec
Murilo Kakazu <yuri.kakazu@gmail.com>
2021-10-25 16:10:21 -0300

    fix: map prediction_processing_time

--- app/service/classification.py
+++ app/service/classification.py
@@ -5,4 +5,5 @@ def classify(capture):
     classification = get_classification(capture['path'])
     capture['prediction_label'] = classification['label']
     capture['prediction_confidence'] = classification['confidence']
+    capture['prediction_processing_time'] = classification['processing_time']
     return capture
